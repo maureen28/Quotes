294653a266ff165404cc6c877765ef46a9cbe3d5
Maureen <murimimaureen69@gmail.com>
2020-03-28 22:11:56 +0300

    import quote class into the components

--- src/app/components/quote-details/quote-details.component.ts
+++ src/app/components/quote-details/quote-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from 'src/app/quote';
 @Component({
   selector: 'app-quote-details',
   templateUrl: './quote-details.component.html',
