2055a222b1a5fdd61cc73c0c5971dabc996d336d
Maureen <murimimaureen69@gmail.com>
2020-03-29 15:06:32 +0300

    add new Quote to quote.ts

--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -6,7 +6,9 @@ import { Quote } from 'src/app/quote';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-
+quotes:Quote[]=[
+  new Quote('The way i see it, if you want the rainbow,you gotta put up with the rain','Dolly Parton','Nimo',new Date(2020/3/4))
+]
   constructor() { }
 
   ngOnInit(): void {
