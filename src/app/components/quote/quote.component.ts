1c1e756369f896904daafb87f4808a719db54111
Maureen <murimimaureen69@gmail.com>
2020-03-29 20:07:18 +0300

    add like & dislike button

--- src/app/components/quote/quote.component.ts
+++ src/app/components/quote/quote.component.ts
@@ -7,8 +7,13 @@ import { Quote } from 'src/app/quote';
 })
 export class QuoteComponent implements OnInit {
 quotes:Quote[]=[
+  new Quote('It takes real skills to trip over nothing,fall up the stairs and choke on air, I have those skills','Anonymous','Moh',new Date(2017/4/13)),
   new Quote('The way i see it, if you want the rainbow,you gotta put up with the rain','Dolly Parton','Nimo',new Date(2020/3/4))
-]
+];
+
+addAQuote(quote){
+  this.quotes.push(quote)
+}
   constructor() { }
 
   ngOnInit(): void {
